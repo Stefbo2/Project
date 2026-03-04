cbe23766897e187c4d91aaa6652a406da4f74b40
StefanBodner <stefan.bodner@hak-kitz.ac.at>
2026-03-04 15:29:08 +0100

    "Add new pages and modify Main.py"

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
-import streamlit as st
+import streamlit as st #py -m streamlit run Main.py
 
-st.title("Hallo Welt")
-st.write("Dies ist eine einfache Streamlit-App, die 'Hallo Welt' anzeigt.")
+st.set_page_config(page_title="HSG - Karteikarten", layout="wide", page_icon=":memo:")
+st.title("HSG - Karteikarten")
+st.write("Willkommen zu deinem Karteikarten-Tool! Hier kannst du deine Lerninhalte organisieren und effektiv lernen. Wähle einen Tab aus, um loszulegen.")
+
+
+
 
